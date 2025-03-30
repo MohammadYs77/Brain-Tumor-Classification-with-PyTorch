8f8ae3c180366496d8af8a64b19e3ebbd64ef01b
Mohammad <mohammad99yousefi@gmail.com>
2025-03-30 18:37:10 +0300

    Reduced Package Dependancies

--- Pipelines/data_pipeline.py
+++ Pipelines/data_pipeline.py
@@ -1,17 +1,15 @@
-import sys
-sys.path.append('D:/AI Courses/Semester 4/Brain Tumor/')
-from configs import RESIZE, BATCH_SIZE
-
 from PIL import Image
 from torch.utils.data import DataLoader, Dataset
 from torchvision import transforms
 
 
 class ImageDataset(Dataset):
-    def __init__(self, df, transform=None, shuffle_data=True):
+    def __init__(self, df, resize, transform=None, shuffle_data=True):
 
         self.df = df
         self.transform = transform
+        self.resize = resize
+        
         if shuffle_data:
             self.df = self.df.sample(frac=1).reset_index(drop=True)
 
@@ -23,19 +21,19 @@ class ImageDataset(Dataset):
         lbl = self.df.iloc[idx, 1]
 
         if self.transform:
-            img = transforms.functional.resize(img, size=[RESIZE, RESIZE], interpolation=transforms.InterpolationMode.BICUBIC)
+            img = transforms.functional.resize(img, size=[self.resize, self.resize], interpolation=transforms.InterpolationMode.BICUBIC)
             img = self.transform(img)
 
         return img, lbl
     
     
-def build_dataloader(tr_df, ts_df, data_transforms):
+def build_dataloader(tr_df, ts_df, resize, batch, data_transforms):
 
-    tr_dt = ImageDataset(tr_df, data_transforms['train'])
-    ts_dt = ImageDataset(ts_df, data_transforms['test'])
+    tr_dt = ImageDataset(tr_df, resize, data_transforms['train'])
+    ts_dt = ImageDataset(ts_df, resize, data_transforms['test'])
 
-    tr_loader = DataLoader(tr_dt, batch_size=BATCH_SIZE, shuffle=True)
-    ts_loader = DataLoader(ts_dt, batch_size=BATCH_SIZE, shuffle=True)
+    tr_loader = DataLoader(tr_dt, batch_size=batch, shuffle=True)
+    ts_loader = DataLoader(ts_dt, batch_size=batch, shuffle=True)
 
     dataloaders = {
         'train': tr_loader,
